543c96c0477efa2151d9cc93cd6439c59a0bc78f
Roberto Casula <casual4free@gmail.com>
2021-01-23 18:05:12 +0100

    fix: changed dev port to be compatible with the webpack server port

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.DIGITAL_LOGIN_SERVICES_PORT || 3000;
+const port = process.env.DIGITAL_LOGIN_SERVICES_PORT || 3003;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
