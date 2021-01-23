4fcf0bce3e376e7b0a85dc0da4d707c2e60b7404
Roberto Casula <casual4free@gmail.com>
2021-01-23 14:10:43 +0100

    chore: wip login form

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+import { StyledLoginForm } from './styled';
+
+export const LoginForm = () => {
+  return (
+    <StyledLoginForm>
+      <TextField
+            required
+            id="outlined-required"
+            label="Required"
+            defaultValue="Hello World"
+            variant="outlined"
+          />
+      <TextField
+        id="outlined-password-input"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        variant="outlined"
+      />
+    </StyledLoginForm>
+  );
+}
