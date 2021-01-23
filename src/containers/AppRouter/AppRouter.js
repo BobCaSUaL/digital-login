450199ccc38e85aa9886530c20800ed6396d6844
Roberto Casula <casual4free@gmail.com>
2021-01-23 23:29:16 +0100

    feat: wired the authentication redirect logic

--- src/containers/AppRouter/AppRouter.js
+++ src/containers/AppRouter/AppRouter.js
@@ -1,18 +1,24 @@
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 
+import PrivateRoute from '../../components/PrivateRoute';
 import Header from '../../components/Header';
 import LoginForm from '../../components/LoginForm';
+
+import { AppContext } from '../App';
 import NotFound from '../NotFound';
 
 export const AppRouter = () => {
+  const { appState } = React.useContext(AppContext);
+
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Header} />
+        <PrivateRoute exact path="/" component={Header} authed={ appState.isAuthenticated }/>
         <Route exact path="/login" component={LoginForm} />
         <Route component={NotFound}  />
       </Switch>
