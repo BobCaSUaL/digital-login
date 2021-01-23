5afa050679a5c12ea89c56bb55e73f7af7b5738e
Roberto Casula <casual4free@gmail.com>
2021-01-23 14:29:51 +0100

    chore: linting fix

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 import { StyledLoginForm } from './styled';
 
@@ -7,12 +8,12 @@ export const LoginForm = () => {
   return (
     <StyledLoginForm noValidate autoComplete="off">
       <TextField
-            required
-            id="outlined-required"
-            label="Required"
-            defaultValue="Hello World"
-            variant="outlined"
-          />
+        required
+        id="outlined-required"
+        label="Required"
+        defaultValue="Hello World"
+        variant="outlined"
+      />
       <TextField
         id="outlined-password-input"
         label="Password"
@@ -20,6 +21,9 @@ export const LoginForm = () => {
         autoComplete="current-password"
         variant="outlined"
       />
+      <Button variant="outlined" color="primary">
+        Login
+      </Button>
     </StyledLoginForm>
   );
 }
