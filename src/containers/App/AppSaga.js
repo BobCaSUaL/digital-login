c389d67fc6689a7555a216dd03ab9c506e79c334
Roberto Casula <casual4free@gmail.com>
2021-01-24 01:59:14 +0100

    feat: app saga jsdoc

--- src/containers/App/AppSaga.js
+++ src/containers/App/AppSaga.js
@@ -1,6 +1,21 @@
 import { actionTypes } from './AppFlux';
 
 export const saga = {
+  /**
+   * Managed the login requested flow.
+   * This saga method performs a request to retieve the current logging-in user.
+   * 
+   * If the login succeeded a LOGIN_SUCCEDED action is dispatched,
+   * a LOGIN_FAILED action is dispatcher otherwise.
+   * 
+   * In addiction you can also provide an `action.meta.$onSuccess.redirectTo`
+   * property if a redirect after login success is required.
+   * The `action.meta.$onSuccess` is added as meta information of the SUCCEEDED
+   * action and can be processed on separate flow.
+   * 
+   * @param {object} action the action from the reducer 
+   * @param {function} dispatch the dispatch function for the current flow
+   */
   async onLoginRequested(action, dispatch) {
     const { payload, meta = {} } = action;
     try {
@@ -9,6 +24,8 @@ export const saga = {
 
       const headers = new Headers();
       headers.append('Authorization', `Bearer ${token}`);
+      // @TODO: create a fetch API wrapper and move the host to a configuration
+      // file or enviromnent variable (applied on build phase)
       const request = new Request('http://localhost:3003/login', {
         method: 'GET',
         headers: headers,
@@ -36,10 +53,24 @@ export const saga = {
     }
   },
 
+  /**
+   * Managed the login failed flow
+   * 
+   * @param {object} action the action from the reducer 
+   * @param {function} dispatch the dispatch function for the current flow
+   */
   async onLoginFailed(action, dispatch) {
     alert('Login failed');
   },
 
+  /**
+   * Process the last added action.
+   * In addiction if an `action.meta.redirectTo` property is provided
+   * than a `$REDIRECT_TO` action will be dispatched.
+   * 
+   * @param {object} action the action from the reducer 
+   * @param {function} dispatch the dispatch function for the current flow
+   */
   async processAction(action, dispatch) {
     switch (action.type) {
       case actionTypes.LOGIN_REQUESTED:
@@ -57,6 +88,12 @@ export const saga = {
     }
   },
 
+  /**
+   * Add an action to this saga.
+   * 
+   * @param {object} action the action from the reducer 
+   * @param {function} dispatch the dispatch function for the current flow
+   */
   addAction(action, dispatch) {
     setImmediate(() => this.processAction(action, dispatch));
   }
